ca85a8f9260650f8eba2a97c3fb562116d10255a
RohiniBalan <144508582+RohiniBalan@users.noreply.github.com>
2024-09-23 16:16:39 +0530

    Add files via upload

---
+++ GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("Branch1");
+	}
+	private void greensAdayar() {
+		System.out.println("Branch2");
+	}
+	private void greensThambaram() {
+		System.out.println("Branch3");
+	}
+	private void greensVelacherry() {
+		System.out.println("Branch4");
+	}
+	private void greensAnnaNagar() {
+		System.out.println("Branch5");
+	}
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensThambaram();
+		g.greensVelacherry();
+		g.greensAnnaNagar();
+	}
+}
