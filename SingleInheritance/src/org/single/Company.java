92ad29fe90458753c00bc694e5eded589c11e3a8
RohiniBalan <144508582+RohiniBalan@users.noreply.github.com>
2024-09-23 16:25:33 +0530

    Add files via upload

---
+++ SingleInheritance/src/org/single/Company.java
@@ -0,0 +1,10 @@
+package org.single;
+//PARENT CLASS
+public class Company {
+	public void companyId() {
+		System.out.println("My company id is 1234XYZ");
+	}
+	public void companyName() {
+		System.out.println("My company name is XYZ");
+	}
+}
