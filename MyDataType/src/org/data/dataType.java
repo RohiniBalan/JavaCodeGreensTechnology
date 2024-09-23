3fe9d8b7ce245f5b0dd32de8006aed54f4e0f7f6
RohiniBalan <144508582+RohiniBalan@users.noreply.github.com>
2024-09-23 16:21:29 +0530

    Add files via upload

---
+++ MyDataType/src/org/data/dataType.java
@@ -0,0 +1,38 @@
+package org.data;
+
+public class dataType {
+	//datatype variablename= value;
+	public static void main(String[] args) {
+		
+		String name="Rohini";
+		//System.out.println("My name is "+name);
+		
+		byte age=20;
+		//System.out.println("My age is "+age);
+		
+		long mobile=1234567809;
+		//System.out.println("My mobie No is "+mobile);
+		
+		char gender='F';
+		//System.out.println("Gender is "+gender);
+		
+		short salary=30000;
+		//System.out.println("My salary is "+salary);
+		
+		int accNo=897654032;
+		//System.out.println("My Account No is "+accNo);
+		
+		float f=3.45f;
+		//System.out.println("Float is "+f);
+		
+		double d=876.123456789072726;
+		//System.out.println("Double is "+d);
+	
+		boolean b=true;
+		//System.out.println("I like Chocolate "+b);
+		
+		//next line=="\n"
+		System.out.println("My name is "+name +"\n"+ "My age is "+age +"\n"+ "My mobie No is "+mobile +"\n"+ "Gender is "+gender +"\n"+ "My salary is "+salary +"\n"+ "My Account No is "+accNo +"\n"+ "Float is "+f +"\n"+ "Double is "+d +"\n"+ "I like Chocolate "+b);
+		
+	}
+}
