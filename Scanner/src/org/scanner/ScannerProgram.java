74df4ab4d6fbcd0966ad3071f2849dc2bc98f2ed
RohiniBalan <144508582+RohiniBalan@users.noreply.github.com>
2024-09-25 11:29:44 +0530

    Add files via upload

---
+++ Scanner/src/org/scanner/ScannerProgram.java
@@ -0,0 +1,34 @@
+package org.scanner;
+import java.util.Scanner;
+public class ScannerProgram {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+//		String next=sc.next();
+//		System.out.println(next);
+//		
+//		String nextLine=sc.nextLine();
+//		System.out.println(nextLine);
+//		
+//		int nextInt=sc.nextInt();
+//		System.out.println(nextInt);
+//		
+//		float nextFloat=sc.nextFloat();
+//		System.out.println(nextFloat);
+//
+//		boolean nextBoolean=sc.nextBoolean();
+//		System.out.println(nextBoolean);
+//		
+//		short nextShort=sc.nextShort();
+//		System.out.println(nextShort);
+//		
+//		long nextLong=sc.nextLong(); 
+//		System.out.println(nextLong);
+//		
+//		byte nextByte=sc.nextByte();
+//		System.out.println(nextByte);
+//		
+		double nextDouble=sc.nextDouble();
+		System.out.println(nextDouble);
+	}
+}
